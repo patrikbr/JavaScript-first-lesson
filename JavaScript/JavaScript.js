ebad1e1629327cb10a4358238c21098f2554be69
Patricia <114097233+patrikbr@users.noreply.github.com>
2022-10-21 20:28:41 +0300

    Add files via upload

---
+++ JavaScript/JavaScript.js
@@ -0,0 +1,30 @@
+const text = '1';
+let text1 = true;
+// text1 = "hey";
+text1= 123;
+
+let score = null;
+let a = 1;
+let plus = "+";
+let b = 2;
+score = a + b;
+
+let idk = "Ala bala portocala";
+
+// alert("Heyyy little human");
+// alert("hehehehe");
+
+// prompt("Cine esti bre")
+
+let nume_prenume = null;
+let nume ="Secret";
+let prenume="Patricia";
+nume_prenume = nume + " " + prenume + " " +"2005";
+console.log(nume_prenume);
+
+
+// console.log(typeof text);
+// console.log(text1);
+
+console.log(a + b);
+console.log(typeof idk);
